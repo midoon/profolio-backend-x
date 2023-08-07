7505f127c27f280c0a50247e3e01499faa85255a
midoon <harunkoding@gmail.com>
2023-08-07 18:28:17 +0700

    Test(portfolio): add update portfolio unit testing

--- tests/portfolio.test.js
+++ tests/portfolio.test.js
@@ -204,3 +204,229 @@ describe("GET /portfolios/:portfolios_id", () => {
     expect(result.body.message).toBe("Forbidden");
   });
 });
+
+//EDIT PORTFOLIO
+describe("PUT /portfolios/:portfolio_id", () => {
+  beforeEach(async () => {
+    await createTestUser();
+  });
+
+  afterEach(async () => {
+    await removeTestUser();
+    await deletePortfolio();
+  });
+
+  it("should can update portfolio with image and other data", async () => {
+    const logedUser = await supertest(app).post("/auth/login").send({
+      password: "12345678",
+      email: "test@gmail.com",
+    });
+
+    const access_token = logedUser.body.data.access_token;
+    const user_id = logedUser.body.data.user_id;
+
+    const jsonData = {
+      title: "test title",
+      tag: "tag,test",
+      link: "test.com",
+      description: "test desc",
+    };
+
+    // Baca konten file sebagai buffer
+    const fs = require("fs");
+    const path = require("path");
+    const fileContent = fs.readFileSync(
+      path.join(__dirname, "/assetsTesting/testImg.png")
+    );
+
+    const createPortfolio = await supertest(app)
+      .post(`/portfolios/`)
+      .set("Authorization", `Bearer ${access_token}`)
+      .attach("image", fileContent, "testImg.png")
+      .field("title", jsonData.title)
+      .field("tag", jsonData.tag)
+      .field("link", jsonData.link)
+      .field("description", jsonData.description);
+
+    const portfolios_id = await getPortfolioId();
+
+    const fileCOntent2 = fs.readFileSync(
+      path.join(__dirname, "/assetsTesting/testImg2.png")
+    );
+
+    const result = await supertest(app)
+      .put(`/portfolios/${portfolios_id}`)
+      .set("Authorization", `Bearer ${access_token}`)
+      .attach("image", fileCOntent2, "testImg2.png")
+      .field("title", jsonData.title)
+      .field("tag", jsonData.tag)
+      .field("link", jsonData.link)
+      .field("description", jsonData.description);
+    await deleteImagePortfolio();
+
+    expect(result.status).toBe(200);
+    expect(result.body.status).toBe(true);
+    expect(result.body.status_code).toBe(200);
+    expect(result.body.message).toBe("Success update portfolio");
+    expect(result.body.data.portfolio_id).toBeDefined();
+  });
+
+  it("should can update portfolio without image", async () => {
+    const logedUser = await supertest(app).post("/auth/login").send({
+      password: "12345678",
+      email: "test@gmail.com",
+    });
+
+    const access_token = logedUser.body.data.access_token;
+    const user_id = logedUser.body.data.user_id;
+
+    const jsonData = {
+      title: "test title",
+      tag: "tag,test",
+      link: "test.com",
+      description: "test desc",
+    };
+
+    // Baca konten file sebagai buffer
+    const fs = require("fs");
+    const path = require("path");
+    const fileContent = fs.readFileSync(
+      path.join(__dirname, "/assetsTesting/testImg.png")
+    );
+
+    const createPortfolio = await supertest(app)
+      .post(`/portfolios/`)
+      .set("Authorization", `Bearer ${access_token}`)
+      .attach("image", fileContent, "testImg.png")
+      .field("title", jsonData.title)
+      .field("tag", jsonData.tag)
+      .field("link", jsonData.link)
+      .field("description", jsonData.description);
+
+    const portfolios_id = await getPortfolioId();
+
+    const fileCOntent2 = fs.readFileSync(
+      path.join(__dirname, "/assetsTesting/testImg2.png")
+    );
+
+    const result = await supertest(app)
+      .put(`/portfolios/${portfolios_id}`)
+      .set("Authorization", `Bearer ${access_token}`)
+      .field("title", jsonData.title)
+      .field("tag", jsonData.tag)
+      .field("link", jsonData.link)
+      .field("description", jsonData.description);
+    await deleteImagePortfolio();
+
+    expect(result.status).toBe(200);
+    expect(result.body.status).toBe(true);
+    expect(result.body.status_code).toBe(200);
+    expect(result.body.message).toBe("Success update portfolio");
+    expect(result.body.data.portfolio_id).toBeDefined();
+  });
+
+  it("should reject if user unauhtorized", async () => {
+    const logedUser = await supertest(app).post("/auth/login").send({
+      password: "12345678",
+      email: "test@gmail.com",
+    });
+
+    const access_token = logedUser.body.data.access_token;
+    const user_id = logedUser.body.data.user_id;
+
+    const jsonData = {
+      title: "test title",
+      tag: "tag,test",
+      link: "test.com",
+      description: "test desc",
+    };
+
+    // Baca konten file sebagai buffer
+    const fs = require("fs");
+    const path = require("path");
+    const fileContent = fs.readFileSync(
+      path.join(__dirname, "/assetsTesting/testImg.png")
+    );
+
+    const createPortfolio = await supertest(app)
+      .post(`/portfolios/`)
+      .set("Authorization", `Bearer ${access_token}`)
+      .attach("image", fileContent, "testImg.png")
+      .field("title", jsonData.title)
+      .field("tag", jsonData.tag)
+      .field("link", jsonData.link)
+      .field("description", jsonData.description);
+
+    const portfolios_id = await getPortfolioId();
+
+    const fileCOntent2 = fs.readFileSync(
+      path.join(__dirname, "/assetsTesting/testImg2.png")
+    );
+
+    const result = await supertest(app)
+      .put(`/portfolios/${portfolios_id}`)
+      .field("title", jsonData.title)
+      .field("tag", jsonData.tag)
+      .field("link", jsonData.link)
+      .field("description", jsonData.description);
+    await deleteImagePortfolio();
+
+    expect(result.status).toBe(403);
+    expect(result.body.status).toBe(false);
+    expect(result.body.status_code).toBe(403);
+    expect(result.body.message).toBe("Forbidden");
+  });
+
+  it("should reject if portfolio not found in DB", async () => {
+    const logedUser = await supertest(app).post("/auth/login").send({
+      password: "12345678",
+      email: "test@gmail.com",
+    });
+
+    const access_token = logedUser.body.data.access_token;
+    const user_id = logedUser.body.data.user_id;
+
+    const jsonData = {
+      title: "test title",
+      tag: "tag,test",
+      link: "test.com",
+      description: "test desc",
+    };
+
+    // Baca konten file sebagai buffer
+    const fs = require("fs");
+    const path = require("path");
+    const fileContent = fs.readFileSync(
+      path.join(__dirname, "/assetsTesting/testImg.png")
+    );
+
+    const createPortfolio = await supertest(app)
+      .post(`/portfolios/`)
+      .set("Authorization", `Bearer ${access_token}`)
+      .attach("image", fileContent, "testImg.png")
+      .field("title", jsonData.title)
+      .field("tag", jsonData.tag)
+      .field("link", jsonData.link)
+      .field("description", jsonData.description);
+
+    const portfolios_id = await getPortfolioId();
+
+    const fileCOntent2 = fs.readFileSync(
+      path.join(__dirname, "/assetsTesting/testImg2.png")
+    );
+
+    const result = await supertest(app)
+      .put(`/portfolios/x`)
+      .set("Authorization", `Bearer ${access_token}`)
+      .field("title", jsonData.title)
+      .field("tag", jsonData.tag)
+      .field("link", jsonData.link)
+      .field("description", jsonData.description);
+    await deleteImagePortfolio();
+
+    expect(result.status).toBe(400);
+    expect(result.body.status).toBe(false);
+    expect(result.body.status_code).toBe(400);
+    expect(result.body.message).toBe("Update Portfolio Error");
+  });
+});
